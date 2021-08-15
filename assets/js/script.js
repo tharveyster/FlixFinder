8928c74c1e29bd2dec3b936ce9e4ab853f3157b9
tharvey <tharveyster@gmail.com>
2021-08-15 09:46:46 -0400

    Refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,18 @@
-var flixTitleInput = document.getElementById('title-input');
-var flixTitleVal = "step brothers";
-var flixSearchBtn = document.getElementById("submitButton")
-var genreSelect = document.querySelector(".button")
-var selectedGenre = document.querySelector(".button").value;
-var flixPosterApi = 'https://img.omdbapi.com/?apikey=d8cda59d'
-var flixInfoApi = 'https://www.omdbapi.com/?apikey=d8cda59d&'
-var flixSearchParr;
-var imdbId;
-var introEl = document.querySelector(".card-intro");
-var movieEl = document.querySelector(".hide");
-var searchHistory = JSON.parse(localStorage.getItem("flixSearch")) || [];
-var searchHistoryEl = document.querySelector("#searchHistory");
+const flixTitleInput = document.getElementById('title-input');
+let flixTitleVal = "";
+const flixSearchBtn = document.getElementById("submitButton")
+let imdbId;
+const searchHistory = JSON.parse(localStorage.getItem("flixSearch")) || [];
 
 // Search the OMDB API for an item that matches the search term
-async function getSearchResults() {
-  flixSearchParr = flixInfoApi + 'plot=full&t=' + flixTitleVal
-  var response = await fetch(flixSearchParr);
-  var data = await response.json();
-  var { Title, Actors, Director, Genre, Rated, Runtime, Year, Plot, Ratings, imdbRating, Poster } = data;
+const getSearchResults = async () => {
+  const flixInfoApi = 'https://www.omdbapi.com/?apikey=d8cda59d&'
+  const introEl = document.querySelector(".card-intro");
+  const movieEl = document.querySelector(".hide");
+  let flixSearchParr = flixInfoApi + 'plot=full&t=' + flixTitleVal
+  let response = await fetch(flixSearchParr);
+  let data = await response.json();
+  const { Title, Actors, Director, Genre, Rated, Runtime, Year, Plot, Ratings, imdbRating, Poster } = data;
   document.getElementById("flix-title").textContent = Title;
   document.getElementById("actors").textContent = Actors;
   document.getElementById("director").textContent = Director;
@@ -40,67 +35,70 @@ async function getSearchResults() {
   if (searchHistory.length === 5) {
     searchHistory.shift();
   }
-  searchHistory.push(flixTitleVal);
-  localStorage.setItem("flixSearch",JSON.stringify(searchHistory));
+  if (!searchHistory.includes(flixTitleVal)) {
+    searchHistory.push(flixTitleVal);
+    localStorage.setItem("flixSearch",JSON.stringify(searchHistory));
+  }
   getSearchHistory();
 }
 
 // Get a trailer clip of the searched item from the YOUTUBE API
-async function getTrailer() {
-  var trailerApi = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=';
-  var trailerApiKey= ' trailer&key=AIzaSyAVo5crd8w8kPuBz_Sq_ebY9N9JoogNUOM'
-  var trailerUrl = trailerApi + flixTitleVal + trailerApiKey;
-  var response = await fetch(trailerUrl);
-  var data = await response.json();
-  var trailer = data.items[0].id.videoId;
-  var youtubeUrl = "https://www.youtube.com/embed/" + trailer;
+const getTrailer = async () => {
+  const trailerApi = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=';
+  const trailerApiKey= ' trailer&key=AIzaSyAVo5crd8w8kPuBz_Sq_ebY9N9JoogNUOM'
+  const trailerUrl = trailerApi + flixTitleVal + trailerApiKey;
+  let response = await fetch(trailerUrl);
+  let data = await response.json();
+  let trailer = data.items[0].id.videoId;
+  const youtubeUrl = "https://www.youtube.com/embed/" + trailer;
   document.getElementById("trailer").setAttribute('src',youtubeUrl);
 }
 
 // Get a list of 3 items that are similar to the searched item from the TMDB API
-async function getRecommendations() {
-  var tmdbUrl = 'https://api.themoviedb.org/3/movie/';
-  var tmdbApiKey = '/similar?api_key=80f80a0f6ffe632acbbd9becc8c87009&language=en-US&page=1';
-  var tmdbApiUrl = tmdbUrl + imdbId + tmdbApiKey;
-  var response = await fetch(tmdbApiUrl);
-  var recommendedFlix = document.querySelector('#similarFlix');
+const getRecommendations = async () => {
+  const tmdbUrl = 'https://api.themoviedb.org/3/movie/';
+  const tmdbApiKey = '/similar?api_key=80f80a0f6ffe632acbbd9becc8c87009&language=en-US&page=1';
+  const tmdbApiUrl = tmdbUrl + imdbId + tmdbApiKey;
+  let response = await fetch(tmdbApiUrl);
+  const recommendedFlix = document.querySelector('#similarFlix');
   if (response.ok) {
-    var data = await response.json();
-    var similarFlix = [];
+    let data = await response.json();
+    let similarFlix = [];
     recommendedFlix.textContent = '';
-    var similarFlixTitle = document.createElement('h3');
+    const similarFlixTitle = document.createElement('h3');
     similarFlixTitle.classList.add("simFlixTitle");
     similarFlixTitle.classList.add("has-text-danger");
     similarFlixTitle.textContent = "Similar Flix";
     recommendedFlix.append(similarFlixTitle);
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       similarFlix[i] = data.results[i].original_title;
-      var similarFlixList = document.createElement('h6');
+      const similarFlixList = document.createElement('h6');
       similarFlixList.textContent = similarFlix[i].toUpperCase();
       recommendedFlix.append(similarFlixList);
     }
   } else {
     recommendedFlix.textContent = '';
-    var similarFlixTitle = document.createElement('h3');
+    const similarFlixTitle = document.createElement('h3');
     similarFlixTitle.classList.add("simFlixTitle");
     similarFlixTitle.classList.add("has-text-danger");
     similarFlixTitle.textContent = "Similar Flix";
     recommendedFlix.append(similarFlixTitle);
-    var similarFlixList = document.createElement('h6');
+    const similarFlixList = document.createElement('h6');
     similarFlixList.textContent = "No similar Flix found!"
     recommendedFlix.append(similarFlixList);
   }
 }
 
-function getSearchHistory() {
+const getSearchHistory = () => {
+  const searchHistoryEl = document.querySelector("#searchHistory");
   searchHistoryEl.textContent = '';
-  var searchHistoryTitle = document.createElement('h3');
+  const searchHistoryTitle = document.createElement('h3');
   searchHistoryTitle.classList.add("simFlixTitle");
   searchHistoryTitle.classList.add("has-text-danger");
   searchHistoryTitle.textContent = "Search History";
   searchHistoryEl.append(searchHistoryTitle);
-  for (var i = 0; i < searchHistory.length; i++) {
-    var searchHistoryList = document.createElement('h6');
+  for (let i = 0; i < searchHistory.length; i++) {
+    const searchHistoryList = document.createElement('h6');
     searchHistoryList.textContent = searchHistory[i].toUpperCase();
     searchHistoryEl.append(searchHistoryList);
   }
